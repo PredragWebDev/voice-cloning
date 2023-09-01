56d67fea04e8f62a901716a726ad3a7cc4150406
PredragWebDev <pedjolinodev@gamil.com>
2023-09-01 15:59:04 +0900

    modify

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,21 +7,23 @@ function LandingPage () {
     const handle_test = async () => {
         alert('okay');
         try {
-            const response = await axios.post('https://play.ht/api/v2/tts', {
-              text: 'Hello from the ultra-realistic voice.',
-              voice: 'larry',
-              quality: 'medium',
-              output_format: 'mp3',
-              speed: 1,
-              sample_rate: 24000,
-            }, {
-              headers: {
-                accept: 'text/event-stream',
-                'content-type': 'application/json',
-              },
-            });
-    
-            console.log(response.data);
+            const response = await axios.post(
+                'https://play.ht/api/v2/tts',
+                {
+                  text: 'Hello World!',
+                  voice: 'larry',
+                },
+                {
+                  headers: {
+                    Authorization: 'Bearer c4b9285f106c3a4e5b4c6794f3a01d26',
+                    'X-USER-ID': 'o9X3lW0h8vNzDQSWRH7CTaRYwVl2',
+                    Accept: 'text/event-stream',
+                    'Content-Type': 'application/json',
+                  },
+                }
+              );
+          
+              console.log(response);
           } catch (error) {
             console.error('error:', error);
           }
